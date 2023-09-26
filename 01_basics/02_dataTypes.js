0d22c2fccd964b42f408bdef836f38be77e8419b
Hardik Jhalani <hardikjhalani23092001@gmail.com>
2023-09-26 21:08:34 +0530

    data types added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer js version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+let name = "Hardik"
+let age = 22
+let isLoggedIn = false
+
+// number => 2^53
+// bigint
+// string => ""
+
+// boolean => true or false
+// null => standalone value
+// undefined => no value assigned
+// symbol => unique
+
+// object
+
+console.log(typeof Symbol); // function
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
