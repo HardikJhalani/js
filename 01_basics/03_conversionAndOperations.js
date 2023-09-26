67895ad3226c7102bb4a9ff4d7e1c77a6dccc066
Hardik Jhalani <hardikjhalani23092001@gmail.com>
2023-09-26 22:10:31 +0530

    operations done

--- 01_basics/03_conversionAndOperations.js
+++ 01_basics/03_conversionAndOperations.js
@@ -23,3 +23,31 @@ console.log(booleanIsLoggedIn); // true
 // 0 => false
 // "" => false
 // "Hardik" => true
+
+// *********************************** Operations ************************************ //
+
+let value = 10
+let negVal = -value
+console.log(negVal); // -10
+
+let str1 = "Hardik"
+let str2 = "Jhalani"
+
+let str3 = str1 + " " + str2
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true); // 1
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+console.log(++gameCounter); // 101
+console.log(gameCounter++); // 101
+console.log(gameCounter); // 102
